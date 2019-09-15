cf080945c18560238b53f2dba83f684b202953ae
Montezi <mirianmontezi.pro@gmail.com>
2019-09-15 00:02:33 -0300

    layout login e register

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '../../assets/logo.png';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  state: Yup.string().required('O estado é obrigatório'),
+  email: Yup.string()
+    .email('Informe um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha deve possuir no mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
+export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Challenge" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="state" placeholder="Seu estado" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+        <button type="submit">Criar Conta</button>
+        <Link to="/"> Já possuo conta</Link>
+      </Form>
+    </>
+  );
+}
